0ab54dc44d5af7b7637f84994ca0510ba1405a47
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-04-30 10:48:31 -0300

    Atividade 30/04
    Ainda falta finalizar o adicionaElemento

--- 20150430.hs
+++ 20150430.hs
@@ -11,3 +11,15 @@ criarFila :: Int -> t -> Failable (t, Fila t)
 criarFila capacidade primeiroElemento
     | capacidade < 1 = Fail "Capacidade menor que 1"
     | otherwise = Works (primeiroElemento, (Element primeiroElemento (Nil) capacidade))
+
+push :: t -> Fila t -> Failable (t, Fila t)
+push novoElemento (Element elemento (fila) capacidade)
+    | capacidade < (contaElementos (Element elemento (fila) capacidade) 0) = Works (novoElemento, (adicionaElemento novoElemento (Element elemento (fila) capacidade)))
+    | otherwise = Fail "Fila cheia"
+
+contaElementos :: Fila t -> Int -> Int
+contaElementos Nil tamanho = tamanho
+contaElementos (Element elemento (fila) capacidade) tamanho = contaElementos (fila) (tamanho+1)
+
+adicionaElemento :: t -> Fila t -> Fila t
+adicionaElemento novoElemento (Element elemento Nil capacidade) = (Element elemento (Element novoElemento Nil capacidade) capacidade)
