76f68624aab72bbb80ab1fcd7a7aedd39216e5e7
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-04-30 10:17:45 -0300

    Atividade 30/04
    Incluindo o tamanho da fila

--- 20150430.hs
+++ 20150430.hs
@@ -1,6 +1,6 @@
 data Failable t = Fail String | Works t
 
-data Fila t = Element t (Fila t) | Nil
+data Fila t = Element t (Fila t) Int | Nil
 
 instance Monad Failable where
  (>>=) (Works x) f = f x
@@ -8,4 +8,4 @@ instance Monad Failable where
  return x = Works x
 
 criarFila :: Int -> t -> Failable (t, Fila t)
-criarFila capacidade primeiroElemento = Works (primeiroElemento, (Element primeiroElemento (Nil)))
+criarFila capacidade primeiroElemento = Works (primeiroElemento, (Element primeiroElemento (Nil) capacidade))
