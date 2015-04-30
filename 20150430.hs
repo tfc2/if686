37b774fd539a0bff75edf8117c303077758d914f
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-04-30 11:54:46 -0300

    Atividade 30/04
    Adicionando o peek, comentando o código

--- 20150430.hs
+++ 20150430.hs
@@ -7,7 +7,7 @@ instance Monad Failable where
  (>>=) (Fail x) _ = Fail x
  return x = Works x
 
-criarFila :: Int -> t -> Failable (t, Fila t)
+criarFila :: Int -> t -> Failable (t, Fila t) -- cria a fila a partir de um elemento
 criarFila capacidade primeiroElemento
     | capacidade < 1 = Fail "Capacidade menor que 1"
     | otherwise = Works (primeiroElemento, (Element primeiroElemento (Nil) capacidade))
@@ -15,7 +15,7 @@ criarFila capacidade primeiroElemento
 -- Ex. 1: criarFila 5 0 -- Works (0,Element 0 Nil 5) 
 -- Ex. 2: criarFila 0 4 -- Fail "Capacidade menor que 1"
 
-push :: t -> Fila t -> Failable (t, Fila t)
+push :: t -> Fila t -> Failable (t, Fila t) -- adiciona o elemento no fim da fila caso haja capacidade para isso
 push novoElemento (Element elemento (fila) capacidade)
     | capacidade > (contaElementos (Element elemento (fila) capacidade) 0) = Works (novoElemento, (adicionaElemento novoElemento (Element elemento (fila) capacidade)))
     | otherwise = Fail "Fila cheia"
@@ -23,24 +23,31 @@ push novoElemento (Element elemento (fila) capacidade)
 -- Ex. 1: push 1 (Element 3 (Element 4 Nil 10) 10) -- Works (1,Element 3 (Element 4 (Element 1 Nil 10) 10) 10)
 -- Ex. 2: push 1 (Element 3 (Element 4 Nil 2) 2) -- Fail "Fila cheia"
 
-contaElementos :: Fila t -> Int -> Int
+contaElementos :: Fila t -> Int -> Int -- retorna o tamanho da fila
 contaElementos Nil tamanho = tamanho
 contaElementos (Element elemento (fila) capacidade) tamanho = contaElementos (fila) (tamanho + 1)
 
 -- Ex. 1: contaElementos (Element 5 (Element 4 Nil 4) 4) 0 -- 2
 -- Ex. 2: contaElementos Nil 0 -- 0
 
-adicionaElemento :: t -> Fila t -> Fila t
+adicionaElemento :: t -> Fila t -> Fila t -- adiciona o elemento no fim da fila
 adicionaElemento novoElemento (Element elemento Nil capacidade) = (Element elemento (Element novoElemento Nil capacidade) capacidade)
 adicionaElemento novoElemento (Element elemento (fila) capacidade) = (Element elemento (adicionaElemento novoElemento fila) capacidade)
 
 -- Ex. 1: adicionaElemento 4 (Element 5 (Element 3 Nil 4) 4) -- Element 5 (Element 3 (Element 4 Nil 4) 4) 4
 -- Ex. 2:adicionaElemento 4 (Element 5 Nil 4) -- Element 5 (Element 4 Nil 4) 4
 
-pop :: Fila t -> Failable (t, Fila t)
+pop :: Fila t -> Failable (t, Fila t) -- remove o primeiro da fila
 pop Nil = Fail "Fila vazia"
 pop (Element elemento fila capacidade) = Works (elemento, fila)
 
 -- Ex. 1: pop Nil -- Fail "Fila vazia"
 -- Ex. 2: pop (Element 5 Nil 4) -- Works (5,Nil)
 -- Ex. 3: pop (Element 5 (Element 4 Nil 4) 4) -- Works (5,Element 4 Nil 4)
+
+peek :: Fila t -> Failable (t, Fila t) -- pega o primeiro da fila sem remover
+peek Nil = Fail "Fila vazia"
+peek (Element elemento fila capacidade) = Works (elemento, (Element elemento fila capacidade))
+
+-- Ex. 1: peek Nil -- Fail "Fila vazia"
+-- Ex. 2: peek (Element 5 (Element 4 Nil 4) 4) -- Works (5,Element 5 (Element 4 Nil 4) 4)
