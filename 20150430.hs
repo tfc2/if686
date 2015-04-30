c70e307366921690fa48a728a7bd59e484c03180
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-04-30 10:19:34 -0300

    Atividade 30/04
    Colocando o show só para testar

--- 20150430.hs
+++ 20150430.hs
@@ -1,6 +1,6 @@
-data Failable t = Fail String | Works t
+data Failable t = Fail String | Works t deriving Show
 
-data Fila t = Element t (Fila t) Int | Nil
+data Fila t = Element t (Fila t) Int | Nil deriving Show
 
 instance Monad Failable where
  (>>=) (Works x) f = f x
