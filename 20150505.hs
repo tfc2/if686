f3e00a64621b504bc7a363e138e796844a98999a
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-05-02 09:00:10 -0300

    Trabalho 11
    Falta corrigir a questão 1, questão 2 feita

--- 20150505.hs
+++ 20150505.hs
@@ -1,5 +1,8 @@
 --- Trabalho 11 ---
 
+import Control.Monad
+import Data.Char
+
 -- Questao 1 --
 
 data Table = Somente [(Int, Int)] | Nada -- Primeiro int = chave, segundo int = valor
@@ -29,3 +32,39 @@ putElem as (x, y)
 removeElem :: Table -> Int -> Table -- int = chave
 removeElem Nothing _ = Nada
 removeElem xs x = Somente [(a, as) | (a, as) <- xs, (a /= x)]
+
+-- Questao 2 --
+
+avaliarAux :: Maybe String -> Bool
+avaliarAux (Just []) = True
+avaliarAux (Just (a:as))
+ | (a == ' ') || (isLetter a)  = avaliarAux (Just as)
+ | otherwise = False
+
+avaliar :: Maybe String -> Maybe String
+avaliar str
+ | avaliarAux str = str
+ | otherwise = Nothing
+
+maiuscula :: Maybe String -> Maybe String
+maiuscula Nothing = Nothing
+maiuscula (Just str) = Just [toUpper s | s <- str]
+
+quebraAux :: String -> String -> [String]
+quebraAux [] ac = [ac]
+quebraAux (a:as) ac
+ | a == ' ' = ac:(quebraAux as [])
+ | otherwise = (quebraAux as (ac++[a]))
+
+quebra :: Maybe String -> [String]
+quebra Nothing = [[]]
+quebra (Just str) = quebraAux str []
+
+main :: IO()
+main = forever $ do  
+    putStr "Digite uma cadeia de caracteres: "  
+    str <- getLine
+    str2 <- return (avaliar (Just str))
+    str3 <- return (maiuscula str2)
+    str4 <- return (quebra str3)
+    mapM_ putStrLn str4
