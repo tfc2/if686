5a086c557f98e17465badd4cfe5db4111eb9efc2
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-05-25 07:12:36 -0300

    comment

--- skimscheme/SSPrettyPrinter.hs
+++ skimscheme/SSPrettyPrinter.hs
@@ -17,6 +17,7 @@ instance Show LispVal where
   show (DottedList h t) = "(" ++ showListContents h ++ " . " ++ show t ++ ")"  
   show (Native p) = "<native procedure>"
   show (Error s) = s
+  show (Comment) = ""
   
 -- This function could be replaced by (unwords.map show). The unwords
 -- function takes a list of String values and uses them to build a 
