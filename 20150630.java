fdde76623a6d3a866ed7a951f588ed6dfc86133c
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-07-01 12:58:31 -0300

    Trabalho 13
    update

--- 20150630.java
+++ 20150630.java
@@ -1,60 +1,75 @@
-package trab13;
+package trabalho13;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Fichas {
-
-	public int[][] repositorios; //10 arrays de fichas
-	public Lock[] locks;
-	int contador; // RECURSO COMPARTILHADO
-
-	public Fichas() {
-		repositorios = new int[10][10000];
-		contador = 0;
-		locks = new ReentrantLock[10];
+	
+	public long contador;
+	public Lock l;
+	
+	public Fichas(){
+		this.contador = 0;
+		l = new ReentrantLock();
 	}
-
-	public int obterFicha(int[][] rep, int[] index) {
-		boolean trava = false;
-		for(int i = 0; (i < 10) && !trava; i++) {
-			trava = locks[i].tryLock();
-			if(trava) {
-				//pegar a chave
-				//como fazer prioridade?
-			} else if (i == 9) {
-				i = 0;
-			}
+	
+	synchronized void obterFicha(){
+		this.l.lock();
+		try{
+			contador++;
+			System.out.println("Obteve ficha " + contador);
+		} finally{
+			this.l.unlock();
 		}
-		return 0;
 	}
-
-
+	
 	public static void main(String[] args) {
 
-		Fichas f = new Fichas();
-
-		//Preencher os arrays de fichas:
-		//Primeira linha = 0, 10, 20...
-		//Segunda linha = 1, 11, 21... Até 99999
-
-		int ficha = 0;
-		for (int coluna = 0; coluna < 10000; coluna++) {
-			for (int linha = 0; linha < 10; linha++) {
-				f.repositorios[linha][coluna] = ficha;
-				ficha++;
+		Fichas fichas = new Fichas();
+		
+		Thread[] threads = new Thread[50]; 
+		
+		MyThread t = new MyThread(fichas);
+		
+		for (int i = 0; i < 50; i ++){															
+			threads[i] = (new Thread(t));
+			threads[i].start();	
+		}
+		
+		for (int i = 0; i < 50; i ++){
+			try {
+				threads[i].join();	
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
+		
 	}
-
 }
 
 class MyThread implements Runnable {
 
-	@Override
+	Fichas fichas;
+	
+	public MyThread (Fichas fichas){
+		this.fichas = fichas;
+	}
+	
 	public void run() {
-
+		for (int i = 0; i < 10; i++){
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			fichas.obterFicha();
+		}
 	}
 
-
 }
+
+/*
+ * Parte teorica aqui
+ * 
+ */
